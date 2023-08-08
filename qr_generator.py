e23d001c6488aff959aa8605c02d089191fa2979
pdxlocations <benlipsey@gmail.com>
2023-08-07 23:19:53 -0700

    Create qr_generator.py

---
+++ qr_generator.py
@@ -0,0 +1,24 @@
+import meshtastic
+import meshtastic.serial_interface
+import pyqrcode
+import os
+
+interface = meshtastic.serial_interface.SerialInterface()
+ourNode = interface.getNode('^local')
+regions = ['US','EU_433','EU_868','CN','JP','ANZ','KR','TW','RU','IN','NZ_865','TH','LORA_24','UA_433','UA_868']
+# regions = ['US','UNSET']
+
+path = 'qrs'
+try: # create a directory, if one doesn't exist
+    os.mkdir(path)
+except OSError as error:
+    print(error)
+
+for region in regions:
+    ourNode.localConfig.lora.region = region
+    url = interface.localNode.getURL(includeAll=True)
+    qr = pyqrcode.create(url)
+    qr.png('qrs/all-channels-'+region+'.png', scale=8)
+    print("File "+ "all-channels-"+region+" saved in directory: "+path)
+
+interface.close()
